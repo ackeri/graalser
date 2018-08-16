59c474bc4d52acd2ab84953d6e8cb8849ea1115c
ackeri <h2ccoch3@gmail.com>
2018-08-16 15:55:56 -0700

    chaning strategies to use graal reflections and possibly custom possibly protobuf compiler

--- Polyglot.java
+++ Polyglot.java
@@ -14,17 +14,42 @@ class Polyglot {
         Context polyglot = Context.create();
         Value v = polyglot.eval("js", "[1,2,42,4]");
  
-        Class c = Class.forName("com.oracle.truffle.api.vm.PolyglotValue$Interop");
 
+        //primitives
+        if(v.isBoolean isNativePointer isNull isNumber isString isHostObject
+                asBoolean asNativePointer asString asHostObject asFloat asLong...
+        //array
+        if(v.hasArrayElements()) {
+          for(int i = 0; i < v.getArraySize(); i++) {
+            Value element = v.getArrayElement(i);
+            write(element);
+          }
+        }
+
+        //members
+        if(v.hasMembers()) {
+          for(String k : v.getMemberKeys()) {
+            Value member = v.getMember(k);
+            write(member);
+          }
+        }
+        
+
+        //Class c = Class.forName("com.oracle.truffle.api.vm.PolyglotValue$Interop");
+        /*
         //initialize serializer
         Kryo kryo = new Kryo();
         kryo.setRegistrationRequired(false);
         kryo.setInstantiatorStrategy(new DefaultInstantiatorStrategy(new StdInstantiatorStrategy())); 
-        register(kryo, v,new String[] {"impl", "asClassLiteral", "rootNode", "sourceVM"});
-        register(kryo, v,new String[] {"impl", "getArrayElement"});
-        register(kryo, v,new String[] {"impl", "languageContext"});
-        register(kryo, v,new String[] {"receiver"});
+        //register(kryo, v,new String[] {"impl", "asClassLiteral", "rootNode", "sourceVM"});
+        //register(kryo, v,new String[] {"impl", "getArrayElement"});
+        //register(kryo, v,new String[] {"impl", "languageContext"});
+        //register(kryo, v,new String[] {"receiver"});
        
+        Object temp = getByName(v, new String[] {"impl", "asClassLiteral", "rootNode", "sourceVM", "contextClassLoader"});
+
+        ClassLoader cl = (ClassLoader)getByName(v, new String[] {"impl"}).getClass().getClassLoader();
+        cl.loadClass("com.oracle.truffle.api.vm.PolyglotValue$Interop");
 
         //Serialize
         Output output = new Output(new FileOutputStream("file.bin"));
@@ -35,6 +60,7 @@ class Polyglot {
         Input input = new Input(new FileInputStream("file.bin"));
         Object object2 = kryo.readClassAndObject(input);
         input.close();
+        */
     }
 
     //Get a field by name (ignoring access control)
@@ -53,12 +79,17 @@ class Polyglot {
         return f.get(host);
     }
 
+    public static Object getByName(Object host, String[] path) throws Exception {
+        Object out = host;
+        for(String s : path) {
+            out = getByName(out, s);
+        }
+        return out;
+    }
+
     //Register alternative serializer for host.(unrolled path)
     public static void register(Kryo kryo, Object host, String[] path) throws Exception{
-        Object temp = host;
-        for(String s : path) {
-          temp = getByName(temp, s);
-        }
+        Object temp = getByName(host, path);
         Class c = temp.getClass();
         kryo.register(c, new ASTSerializer<>(kryo,c));
     }
