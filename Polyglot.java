e8d471dc8d40921b42ad4878ff7b64a5083e8db0
ackeri <h2ccoch3@gmail.com>
2018-08-22 13:55:09 -0700

    added complex java example

--- Polyglot.java
+++ Polyglot.java
@@ -18,6 +18,7 @@ class Polyglot {
         Value v4 = polyglot.eval(Source.newBuilder("js", new File("./complexclassdef.js")).build());
         //Value v5 = polyglot.eval(Source.newBuilder("python", new File("./class.py")).build());
         //v5.getMember("mutate").execute();
+        Value v6 = polyglot.asValue(ComplexClass.Init());
 
 				TypesDB.register(v1);
 				TypesDB.register(v2);
@@ -26,8 +27,11 @@ class Polyglot {
         TypesDB.register(v4.getMember("child1"));
         TypesDB.register(v4.getMember("child1").getMember("grandchild"));
         //TypesDB.register(v5);
+        TypesDB.register(v6);
+        TypesDB.register(v6.getMember("child1"));
+        TypesDB.register(v6.getMember("child1").getMember("gc"));
         
-				Value v = v4;
+				Value v = v6;
 
 				System.out.println("--Writing--");
 				Writer w = new Writer(new FileOutputStream("file.bin"));
@@ -70,4 +74,38 @@ class Polyglot {
 			return fielda + " " + fieldb;
 		}
 	}
+
+  public static class ComplexClass extends ArrayList<Integer> {
+    public InnerClass child1;
+    public InnerClass child2;
+    
+    public ComplexClass construct() {
+        return new ComplexClass();
+    }
+    public static ComplexClass Init() {
+        ComplexClass out = new ComplexClass();
+        out.child1 = new InnerClass();
+        out.child2 = new InnerClass();
+        out.child1.gc = new GrandchildClass();
+        out.child2.gc = out.child1.gc;
+        out.child1.gc.val = 5;
+        return out;
+    }
+    public String toString() {
+        return "gc equal? " + (child1.gc == child2.gc);
+    }
+
+    public static class InnerClass {
+        public GrandchildClass gc;
+        public InnerClass construct() {
+            return new InnerClass();
+        }
+    }
+    public static class GrandchildClass {
+        public int val;
+        public GrandchildClass construct() {
+            return new GrandchildClass();
+        }
+    }
+  }
 }
